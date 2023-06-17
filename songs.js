4a5e256b801479aecb92c4fc97136ad3aacdee66
Cicero Cauã <126025158+Caua017@users.noreply.github.com>
2023-06-17 20:42:59 -0300

    commit - first

---
+++ songs.js
@@ -0,0 +1,25 @@
+export default[
+    {
+        src: "./src/Teto - Manha.mp3 ",
+        name: "Teto - Manha",
+
+    },
+
+    {
+        src: "./src/Jesus - Chorou.mp3",
+        name: "Racionais - Jesus Chorou",
+    },
+
+    {
+        src: "./src/Serenata.mp3",
+        name: "Grilo - Serenata Existêncialista",
+    },
+
+    {
+        src: "./src/Everlong.mp3",
+        name: "Foo Fighters - Everlong",
+
+    }
+
+
+];
